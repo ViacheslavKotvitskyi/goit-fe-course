cd083450ac0627d2ed57e96e85cb4b27098b2594
ViacheslavKotvitskyi <kot.bsys@gmail.com>
2019-04-25 09:12:21 +0300

    JS_HW3_Rev.1

--- javascript/module-03/app.js
+++ javascript/module-03/app.js
@@ -3,27 +3,19 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
 const isLoginValid = function(login) {
-  if (login.length >= 4 && login.length <= 16) {
-    return true;
-  }
-  return false;
+  return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function(allLogins, login) {
-  for (const i of allLogins) {
-    if (i === login) {
-      return false;
-    }
-  }
-  return true;
+  return allLogins.includes(login);
 };
 
 const addLogin = function(allLogins, login) {
   if (!isLoginValid(login)) {
-    return 'Ощибка! Логин должен быть от 4 до 16 символов';
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
   }
 
-  if (!isLoginUnique(allLogins, login)) {
+  if (isLoginUnique(allLogins, login)) {
     return 'Такой логин уже используется!';
   }
   allLogins.push(login);
